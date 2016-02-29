dad7132b31d1eed2430108901d30e8218c7d8158
Florian Pircher <git@leuchtschirm.com>
2016-02-29 09:33:15 +0100

    Switch to Standard HTML and JavaScript

--- source/www/script.js
+++ source/www/script.js
@@ -1,6 +1,1 @@
-window.addEventListener("keydown",function(event){
-    if(event.keyCode==123)
-    {
-        require('nw.gui').Window.get().showDevTools();
-    }
-})
+console.log('Running JavaScript.')
