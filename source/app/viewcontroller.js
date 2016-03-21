fe3a817798b5e63c405b3e5ad951da65a0783ea5
Florian Pircher <git@leuchtschirm.com>
2016-03-21 15:25:48 +0100

    Implement Select Tab Events

--- source/app/viewcontroller.js
+++ source/app/viewcontroller.js
@@ -4,6 +4,32 @@
   
   self.views = []
   self.activeView = null
+  self.activeTable = null
+  self.selectTabEvents = []
+  
+  // ===============
+  // = Select View =
+  // ===============
+  
+  var triggerSelectTabEvents = function (tabId) {
+    tabName = tabId.slice(5)
+    
+    
+    self.selectTabEvents.filter(function (x) {
+      return x.tabName === tabName
+    }).forEach(function (x) {
+      x.action()
+    })
+  }
+  
+  self.selectTabEvents.push({ tabName: 'order', action: function () {
+    // Load self.activeTable
+    
+  }})
+  
+  // ========
+  // = Init =
+  // ========
   
   var views = document.getElementsByClassName('view')
   
@@ -16,14 +42,21 @@
       x.setAttribute('hidden', 'hidden')
     } else {
       self.activeView = x
-      document.getElementById('nav-'+self.activeView.id).classList.add('active')
+      document.getElementById('nav-'+x.id).classList.add('active')
+      triggerSelectTabEvents(x.id)
     }
   })
   
+  // ==========
+  // = Events =
+  // ==========
+  
   window.addEventListener('hashchange', function () {
-    var hash = location.hash.slice(1)
+    var hash = location.hash
+    
+    var id = hash.slice(1)
     var oldView = self.activeView
-    var newView = document.getElementById(hash)
+    var newView = document.getElementById(id)
     
     oldView.setAttribute('hidden', 'hidden')
     newView.removeAttribute('hidden')
@@ -31,7 +64,11 @@
     document.getElementById('nav-'+oldView.id).classList.remove('active')
     document.getElementById('nav-'+newView.id).classList.add('active')
     
+    triggerSelectTabEvents(newView.id)
+    
     self.activeView = newView
+    
+    history.pushState('', document.title, location.pathname)
   })
   
 }())
