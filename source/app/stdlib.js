f0a798a1b6a8000c540d0a98719529da672d3861
Florian Pircher <git@leuchtschirm.com>
2016-03-23 21:47:13 +0100

    Add stdlib

---
+++ source/app/stdlib.js
@@ -0,0 +1,31 @@
+function OrderList(id) {
+  this.id = id || -1
+  this.orders = []
+}
+
+function Order(id, productId, name, extras, price) {
+  this.id = id || -1
+  this.prod_id = productId
+  this.name = name
+  this.extras = extras
+  this.price = price
+  this.stats = 'none'
+  this.statusCode = 0
+}
+
+Order.prototype.copy = function () {
+  return Order(this.id, this.productId, this.name, this.extras, this.price)
+}
+
+function Extra(id, name, price, action) {
+  this.id = id || -1
+  this.name = name || 'Senza nome'
+  this.price = price || 0
+  this.action = action || '+'
+}
+
+function Table(id, name) {
+  this.id = id || -1
+  this.name = name || `Tavolo ${id}`
+  this.pending = []
+}
