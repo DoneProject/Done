36930aed830771e38178fa4e7629c1547bc5f67c
Florian Pircher <git@leuchtschirm.com>
2016-03-31 11:54:58 +0200

    Update Done.js

--- source/app/done.js
+++ source/app/done.js
@@ -1,63 +1,65 @@
 var Done = (function(baseSocketURL) {
   
-  var self = this
-  
-  // =============
-  // = Properies =
-  // =============
-  
-  self.DONE_AUTH_HEADER = 'DoneAuth'
-  
-  self.baseSocketURL = baseSocketURL
-  self.connection = new WebSocket(self.baseSocketURL)
-  self.callback = function (event, success) { }
-  
-  // ========
-  // = Auth =
-  // ========
-  
-  self.auth = function (accessToken) {
-    self.accessToken = accessToken
+  function Done(baseSocketURL) {
+    var self = this
+    
+    // =============
+    // = Properies =
+    // =============
+    
+    self.DONE_AUTH_HEADER = 'DoneAuth'
+    
+    self.baseSocketURL = baseSocketURL
+    self.connection = new WebSocket(self.baseSocketURL)
+    self.callback = function (event, success) { }
+    
+    // ========
+    // = Auth =
+    // ========
+    
+    self.auth = function (accessToken) {
+      self.accessToken = accessToken
+    }
+    
+    // ========
+    // = POST =
+    // ========
+    
+    self.post = function (message, data) {
+      return self.connection.send(JSON.stringify({ post: message, data: data }))
+    }
+    
+    self.postTables    = function (data, callback) { return self.post('tables', data, callback) }
+    self.postOrderable = function (data, callback) { return self.post('orderable', data, callback) }
+    self.postExtras    = function (data, callback) { return self.post('extras', data, callback) }
+    self.postQueue     = function (data, callback) { return self.post('queue', data, callback) }
+    
+    // =======
+    // = GET =
+    // =======
+    
+    self.get = function (message, callback) {
+      self.connection.send(JSON.stringify({ get: message }))
+    }
+    
+    self.getTables    = function (callback) { return self.get('tables', callback) }
+    self.getOrderable = function (callback) { return self.get('orderable', callback) }
+    self.getExtras    = function (callback) { return self.get('extras', callback) }
+    self.getQueue     = function (callback) { return self.get('queue', callback) }
+    
+    // ==================
+    // = Event Listener =
+    // ==================
+    
+    self.connection.addEventListener('message', function (event) {
+      return self.callback(event, true)
+    })
+    
+    self.connection.addEventListener('error', function (event) {
+      return self.callback(event, false)
+    })
   }
   
-  // ========
-  // = POST =
-  // ========
+  return new Done(baseSocketURL)
   
-  self.post = function (message, data) {
-    return self.connection.send(JSON.stringify({ post: message, data: data }))
-  }
-  
-  self.postTables    = function (data) { return self.post('tables', data) }
-  self.postOrderable = function (data) { return self.post('orderable', data) }
-  self.postExtras    = function (data) { return self.post('extras', data) }
-  self.postQueue     = function (data) { return self.post('queue', data) }
-  
-  // =======
-  // = GET =
-  // =======
-  
-  self.get = function (message) {
-    return self.connection.send(JSON.stringify({ get: message }))
-  }
-  
-  self.getTables    = function () { return self.get('tables') }
-  self.getOrderable = function () { return self.get('orderable') }
-  self.getExtras    = function () { return self.get('extras') }
-  self.getQueue     = function () { return self.get('queue') }
-  
-  // ==================
-  // = Event Listener =
-  // ==================
-  
-  self.connection.addEventListener('message', function (event) {
-    return self.callback(event, true)
-  })
-  
-  self.connection.addEventListener('error', function (event) {
-    return self.callback(event, false)
-  })
-  
-  return self
-  
-}('ws://localhost'))
+}('ws://mbp.local:8181'))
